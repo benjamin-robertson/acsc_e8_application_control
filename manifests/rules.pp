73941f1286298f1e4fe3a408da2c5a4397197860
Benjamin Robertson <ben.robertson@puppet.com>
2021-09-24 15:44:52 +1000

    No more *

--- manifests/rules.pp
+++ manifests/rules.pp
@@ -10,8 +10,16 @@ class puppet_applocker_fervid::rules (
 
   # Apply applocker rules
   $applocker_rules.each | String $name, Hash $rule | {
-    #applocker_rule { $name:
-    #  * => $rule
-    #}
+    applocker_rule { $name:
+      ensure            => $rule['ensure'],
+      action            => $rule['action'],
+      conditions        => $rule['conditions'],
+      exceptions        => $rule['exceptions'],
+      description       => $rule['description'],
+      mode              => $rule['mode'],
+      rule_type         => $rule['rule_type'],
+      type              => $rule['type'],
+      user_or_group_sid => $rule['user_or_group_sid'],
+    }
   }
 }
