7440a1e25fb71d0cec0a06e51e4fdd6aa2a27416
Benjamin Robertson <ben.robertson@puppet.com>
2021-09-24 17:04:49 +1000

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,11 @@
 #   include puppet_applocker_fervid
 class puppet_applocker_fervid (
   Hash $additional_applocker_rules = {},
+  Enum['Audit','Disabled','Enabled'] $executable_rules = 'Enabled',
+  Enum['Audit','Disabled','Enabled'] $msi_rules = 'Enabled',
+  Enum['Audit','Disabled','Enabled'] $dll_rules = 'Enabled',
+  Enum['Audit','Disabled','Enabled'] $script_rules = 'Enabled',
+  Enum['Audit','Disabled','Enabled'] $packaged_app_rules = 'Enabled',
 ) {
   # lookup default rules
   $default_applocker_rules = lookup(puppet_applocker_fervid::default_applocker_rules)
