416f34ea3067da4366eac96740a27c464eaed5c4
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-01 13:28:37 +1100

    Fixed rspec errors

--- spec/classes/rule_status_spec.rb
+++ spec/classes/rule_status_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 describe 'puppet_applocker_fervid::rule_status' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:pre_conditon) { 'include puppet_applocker_fervid' }
+      let(:params) { {'executable_rules' => 'Enabled', 'msi_rules' => 'Enabled', 'dll_rules' => 'Enabled', 'script_rules' => 'Enabled', 'packaged_app_rules' => 'Enabled'}}
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
