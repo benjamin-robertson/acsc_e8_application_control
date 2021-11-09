0802c54674b25a0cab6000866c53b1717146d4ab
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-09 23:50:07 +1100

    remove notify

--- manifests/rules.pp
+++ manifests/rules.pp
@@ -34,7 +34,6 @@ class acsc_e8_application_control::rules (
     }
   } elsif $executable_rules == 'Audit' {
     $exec_applocker_rules.each | String $name, Hash $rule | {
-      notify { "Audit rule ran ${name}":}
       applocker_rule { $name:
         ensure            => $rule['ensure'],
         action            => $rule['action'],
