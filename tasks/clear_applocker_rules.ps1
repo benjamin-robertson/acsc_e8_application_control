806845ce36ecf27780eff7531a10d0e9a88470d8
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-09 23:43:45 +1100

    removed tasks

--- tasks/clear_applocker_rules.ps1
+++
@@ -1,9 +0,0 @@
-echo '<AppLockerPolicy Version="1">' > applock.xml
-echo '<RuleCollection Type="Appx" EnforcementMode="NotConfigured" />' >> applock.xml
-echo '<RuleCollection Type="Exe" EnforcementMode="NotConfigured" />' >> applock.xml
-echo '<RuleCollection Type="Msi" EnforcementMode="NotConfigured" />' >> applock.xml
-echo '<RuleCollection Type="Script" EnforcementMode="NotConfigured" />' >> applock.xml
-echo '<RuleCollection Type="Dll" EnforcementMode="NotConfigured" />' >> applock.xml
-echo '</AppLockerPolicy>' >> applock.xml
-Set-AppLockerPolicy -XMLPolicy applock.xml
-rm applock.xml
